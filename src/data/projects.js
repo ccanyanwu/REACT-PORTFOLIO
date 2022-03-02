4d27cefa734c8d014edbdf552b36a387771621f6
ccanyanwu <eme42c@gmail.com>
2022-03-02 11:50:17 +0100

    fix: update contact and feature

--- src/data/projects.js
+++ src/data/projects.js
@@ -16,7 +16,7 @@ export const Projects = [
     image:
       "https://res.cloudinary.com/trive/image/upload/f_auto,q_auto:eco/v1645783870/muvlist.png",
     description:
-      "Muvlist helps you find the latest trending and popular movies. I used themoviedatabase api to fetch movie details. I was always undecided on what movie to watch when I want to relax for the weekends and this helped me fix that problem and gain more mastery with react and the react custom hooks",
+      "Muvlist helps you find the latest trending and popular movies. I used theMovieDataBase api to fetch movie details. I was always undecided on what movie to watch when I want to relax for the weekends and this helped me fix that problem and gain more mastery with react and the react custom hooks",
     tools: ["ReactJs", "Axios", "CSS3"],
     github: "https://github.com/ccanyanwu/MOVIE-LISTING-APP-REACT-",
     live: "https://muvlist.netlify.app",
@@ -28,7 +28,7 @@ export const Projects = [
       "https://res.cloudinary.com/trive/image/upload/f_auto,q_auto:eco/v1645784692/github.png",
     description:
       "Github finder helps users get the info of any person registered on Github by simply typing in their username. This project helped me understand the usage of the contextAPI and useReducer hook for state management. ",
-    tools: ["ReactJs", "Tailwind CSS", "Axios", "GithubAPI"],
+    tools: ["ReactJs", "Tailwind CSS", "Axios", "GithubRESTAPI"],
     github: "https://github.com/ccanyanwu/GITHUB-FINDER-APP---REACT",
     live: "https://githubidentifier.netlify.app",
   },
@@ -38,7 +38,7 @@ export const Projects = [
     image:
       "https://res.cloudinary.com/trive/image/upload/f_auto,q_auto:eco/v1645785611/fido.png",
     description:
-      "Github finder helps users get the info of any person registered on Github by simply typing in their username. This project helped me understand the usage of the contextAPI and useReducer hook for state management. ",
+      "With Fido Poultry Management System app, farm owners no longer have to worry about losses in the business and livestock as everything is duly accounted for daily. This project was built with vanilla javascript and exercised the power of the browser's local storage for storing data.",
     tools: ["Javascript", "CSS3"],
     github: "https://github.com/ccanyanwu/FIDO-POULTRY",
     live: "https://fidopoultry.netlify.app",
@@ -49,8 +49,8 @@ export const Projects = [
     image:
       "https://res.cloudinary.com/trive/image/upload/f_auto,q_auto:eco/v1645786473/expenditure.png",
     description:
-      "Github finder helps users get the info of any person registered on Github by simply typing in their username. This project helped me understand the usage of the contextAPI and useReducer hook for state management. ",
-    tools: ["NodeJS", "ExpressJs"],
+      "Expenditure tracker app helps managers in the company's  departments manage and keep a record of all company expenses spent by employees. The type of expense, category, date, time and much more can be tracked by managers using this app. There is also of role based access in place, giving managers privileges review expenses.",
+    tools: ["NodeJS", "ExpressJs", "Bootstrap"],
     github: "https://github.com/ccanyanwu/EXPENSE-TRACKING-APPLICATION",
     live: "https://expendituretracker.herokuapp.com/",
   },
@@ -60,18 +60,18 @@ export const Projects = [
     image:
       "https://res.cloudinary.com/trive/image/upload/f_auto,q_auto:eco/v1645787835/football.png",
     description:
-      "Github finder helps users get the info of any person registered on Github by simply typing in their username. This project helped me understand the usage of the contextAPI and useReducer hook for state management. ",
-    tools: ["javaScript"],
+      "A simple football quiz app built with javascript that tries to mimic single page application behaviour. It was one of my earliest javascript prjects that taught me a lot about the capabilities of javascript.",
+    tools: ["javaScript", "CSS3"],
     github: "https://github.com/ccanyanwu/FOOTBALL-QUIZ-APP",
     live: "https://ccanyanwu.github.io/FOOTBALL-QUIZ-APP/",
   },
   {
     stack: ["all", "javascript"],
-    name: "Calculator",
+    name: "Quik Calc",
     image:
       "https://res.cloudinary.com/trive/image/upload/f_auto,q_auto:eco/v1645788240/calculator.png",
     description:
-      "Github finder helps users get the info of any person registered on Github by simply typing in their username. This project helped me understand the usage of the contextAPI and useReducer hook for state management. ",
+      "Quik Calc can be used in performing basic mathematical operations such as addition, division, multiplication and subtraction. On an attempt to make wrong operations, an error is displayed to the user of a few seconds. It can also take large numbers as input for operations. This app was built with vanilla javascript",
     tools: ["javaScript", "CSS3"],
     github:
       "https://github.com/ccanyanwu/IGhub-Frontend-Assignment-/tree/Calcagba",
@@ -83,7 +83,7 @@ export const Projects = [
     image:
       "https://res.cloudinary.com/trive/image/upload/f_auto,q_auto:eco/v1645814385/heavyweight.jpg",
     description:
-      "Github finder helps users get the info of any person registered on Github by simply typing in their username. This project helped me understand the usage of the contextAPI and useReducer hook for state management. ",
+      "Heavyweight landing page is a pixel perfect clone of the famous heavyweight intuitive interfaces for startup website (https://heavyweight.nl) landing page. Built with bootstrap, this project really improved my knowledge of handling lay out for webpages and responsive web design too.",
     tools: ["html", "Bootstrap"],
     github:
       "https://github.com/ccanyanwu/IGhub-Frontend-Assignment-/tree/heavyWeight",
@@ -95,7 +95,7 @@ export const Projects = [
     image:
       "https://res.cloudinary.com/trive/image/upload/f_auto,q_auto:eco/v1645812665/hi-praise.png",
     description:
-      "Github finder helps users get the info of any person registered on Github by simply typing in their username. This project helped me understand the usage of the contextAPI and useReducer hook for state management. ",
+      "This project was born out of the desire to give a local business that has been in existence for 16 years an online presence. With this webpage, customers can place their order after going through the company's products. The location on the map is also available for easy direction to the physical facility.",
     tools: ["html", "CSS3", "AOS"],
     github: "https://github.com/ccanyanwu/Hi-praise",
     live: "https://hi-praise.netlify.app",
