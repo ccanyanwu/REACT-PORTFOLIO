f556efc5057265064f8544e544049f21c3e14b45
ccanyanwu <eme42c@gmail.com>
2022-03-14 15:53:46 +0100

    feat: update App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  useLocation,
 } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
